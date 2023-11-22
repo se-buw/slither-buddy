eb0c635544a956cf0f29ae083f7502a064147f25
Adam <adam.alshourbagi@gmail.com>
2023-11-22 18:32:42 +0100

    updated the snake class. Added a proper graphical user interface and background. set up gameloop.

--- code/javafx-gradle-template-main/src/main/java/de/buw/i2p/Snake.java
+++ code/javafx-gradle-template-main/src/main/java/de/buw/i2p/Snake.java
@@ -1,24 +1,31 @@
 package de.buw.i2p;
+import javafx.geometry.Point2D;
+
 import java.awt.Point;
 import java.util.ArrayList;
 
 public class Snake {
     private final int size_;
+    private final int segmentSize_;
     private int speed_;
     private int xDirection_;
     private int yDirection_;
     // buffers next direction till snake reaches end of tile;
     private int nextYDirection_;
     private int nextXDirection_;
-    private ArrayList<Point> snakeSegments_;
+    private ArrayList<Point2D> snakeSegments_;
 
-    public Snake(int size, int speed, Point head, int x_direction, int y_direction) {
+    public Snake(int size, int segmentSize, int speed, Point2D head, int x_direction, int y_direction) {
         this.size_ = size;
+        segmentSize_ = segmentSize;
         this.speed_ = speed;
         this.xDirection_ = x_direction;
         this.yDirection_ = y_direction;
         this.snakeSegments_ = new ArrayList<>();
         snakeSegments_.add(head);
+        for (int i = 1; i < size ; i++){
+            snakeSegments_.add(new Point2D(head.getX() - x_direction * segmentSize_ * i, head.getY() - yDirection_ * segmentSize_ * i));
+        }
     }
 
     public void setSpeed(int speed) {
