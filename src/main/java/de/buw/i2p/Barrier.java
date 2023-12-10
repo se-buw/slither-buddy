d601c1a993a2e1836bee2441af3a4e4ca2fbf863
Christphr596 <christphr6483@gmail.com>
2023-12-10 17:07:35 +0100

    commited

--- src/main/java/de/buw/i2p/Barrier.java
+++ src/main/java/de/buw/i2p/Barrier.java
@@ -6,13 +6,13 @@ import java.util.Random;
 import java.awt.Point;
 import java.util.ArrayList;
 
-public class Barrier {
+public class Barrier {//new barrierclass
 
     private Color color = Color.GREENYELLOW;
     private int segmentSize;
     private int pos_x;
     private int pos_y;
-
+//position of left upper corner
 
     public Barrier(int segmentSize){
         this.segmentSize = segmentSize;
@@ -20,9 +20,9 @@ public class Barrier {
     void place_barrier(){
         Random random = new Random();
 
-        int x = random.nextInt(19);
+        int x = random.nextInt(19);//random value between 0 and 19
         int y = random.nextInt(19);
-
+        //prevents barriers for beeing set on a field where one of the snakes starts
         if (x == 5 || x == 15 && y <= 15 && y >= 9){
             x =+ 1;
         }
@@ -30,7 +30,7 @@ public class Barrier {
         pos_x = x;
         pos_y = y;
     }
-
+//draws the Barrier on the barriercanvas, with given values * segmentsize
     void draw_barrier(GraphicsContext gc){
         gc.setFill(color);
         gc.fillRect(pos_x * segmentSize, pos_y * segmentSize, segmentSize, segmentSize);
@@ -39,7 +39,4 @@ public class Barrier {
     boolean collision_barrier(int head_x, int head_y){
         return (pos_x == head_x && pos_y == head_y);
     }
-
-
-
 }
